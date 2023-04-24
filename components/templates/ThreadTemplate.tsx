938c7f0fcc70e2d4cee3ae169144f6651c97cdb9
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-04-24 19:14:11 +0000

    [fix] grid template

--- components/templates/ThreadTemplate.tsx
+++ components/templates/ThreadTemplate.tsx
@@ -29,13 +29,13 @@ const ThreadTemplate: FC<TthreadProps> = ({ platformLogo, platform, isLoading, p
                                 <Image src={`/assets/${platformLogo}`} alt={platform} width={50} height={50} className="w-[45px] rounded-full h-[45px] object-cover" />
                             </div>
                             <p style={{ gridTemplateRows: "1fr 1fr" }} className="my-3">{postContent}</p>
-                            <div className='grid grid-cols-2 gap-1' id={(showwcasePostImages?.length === 3 || twitterPostImages?.length === 3) ? "three-images" : ""} >
+                            <div className={`grid  grid-cols-2 gap-1`} id={(showwcasePostImages?.length === 3 || twitterPostImages?.length === 3) ? "three-images" : ""} >
 
 
                                 {
                                     platform === TABS[0] ?
                                         (showwcasePostImages && showwcasePostImages.map((postImage) => (
-                                            <img key={postImage} src={postImage} alt="post_image" loading='lazy' className={`bg-gray-500  w-full rounded-md  border object-cover `} />
+                                            <img key={postImage} src={postImage} alt="post_image" loading='lazy' className={`bg-gray-500  w-full rounded-md  border object-cover ${(showwcasePostImages?.length === 2 || twitterPostImages?.length === 2) ? "h-[280px]" :" max-h-[370px] "}`} />
                                         ))) :
                                         (twitterPostImages && twitterPostImages.map((twitterPostImage) => {
                                             const { url, preview_image_url } = twitterPostImage
