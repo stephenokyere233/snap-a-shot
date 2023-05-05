91b933f361846cb30cb7a6002d5737a937eb8410
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-05-05 06:05:50 +0000

    [fix] TS error in copy code

--- pages/index.tsx
+++ pages/index.tsx
@@ -151,40 +151,49 @@ export default function Home() {
   }
 
   const copyImage = () => {
-    const shotElement = document.querySelector('#shot');
-    if (!shotElement) return
+    const shotElement = document.querySelector('#shot') as HTMLElement | null;
+    if (!shotElement) return;
 
     const canvasElement = document.createElement('canvas');
     const context = canvasElement.getContext('2d');
 
-    const toastId = toast.loading("Copying...")
-    htmlToImage.toPng(shotElement as HTMLElement)
-      .then(function (dataUrl) {
+    if (!context) {
+      console.error('Unable to create canvas context');
+      return;
+    }
+
+    const toastId = toast.loading("Copying...");
+
+    htmlToImage.toPng(shotElement)
+      .then(function (dataUrl: string) {
         const imageElement = new Image();
         imageElement.onload = function () {
           canvasElement.width = imageElement.width;
           canvasElement.height = imageElement.height;
-          context?.drawImage(imageElement, 0, 0);
+          context.drawImage(imageElement, 0, 0);
           canvasElement.toBlob(function (blob) {
+            if (!blob) {
+              console.error('Unable to create blob');
+              return;
+            }
             const item = new ClipboardItem({ "image/png": blob });
             navigator.clipboard.write([item]).then(function () {
               toast.success('Saved to clipboard');
-              toast.dismiss(toastId)
+              toast.dismiss(toastId);
             }, function (err) {
               toast.error('Error saving to clipboard: ', err);
-              toast.dismiss(toastId)
+              toast.dismiss(toastId);
             });
           });
         };
         imageElement.src = dataUrl;
+      })
+      .catch(function (error) {
+        console.error('Error copying image:', error);
+        toast.error('Error copying image:', error);
+        toast.dismiss(toastId);
       });
-
-    //   htmlToImage.toPng(shotElement as HTMLElement)
-    //     .then(function (dataUrl) {
-    //       navigator.clipboard.writeText(dataUrl);
-    //       console.log('Saved to clipboard');
-    //     });
-  }
+  };
 
   const fetchTwitterPost = async (twitterPostID: string) => {
     setLoading(true)
