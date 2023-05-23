55b4c38abe7e934bd0336dbf5cef1fcc4890c9d4
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-05-23 06:13:28 +0000

    [add[ cleanup and refactor

--- context/index.tsx
+++ context/index.tsx
@@ -1,3 +1,4 @@
+import { TABS } from "@/constants/tabs"
 import { IAppContext, IAppProvider } from "@/interfaces"
 import React, { useState } from "react"
 
@@ -8,9 +9,10 @@ const AppProvider: React.FC<IAppProvider> = ({ children }) => {
     const [isShowwcaseDataFetched, setIsShowwcaseDataFetched] = useState<boolean>(false)
     const [isTwitterDataFetched, setIsTwitterDataFetched] = useState<boolean>(false)
     const [selectedBGColor, setSelectedBGColor] = useState<string>("linear-gradient(to right, #4776e6, #8e54e9)")
-    const [showStats,setShowStats]=useState<boolean>(true)
+    const [showStats, setShowStats] = useState<boolean>(true)
+    const [platform, setPlatform] = useState<string>(TABS[0])
     return (
-        <AppContext.Provider value={{ isShowwcaseDataFetched, setIsShowwcaseDataFetched, isTwitterDataFetched, setIsTwitterDataFetched, selectedBGColor, setSelectedBGColor, showStats, setShowStats }}>
+        <AppContext.Provider value={{ isShowwcaseDataFetched, setIsShowwcaseDataFetched, isTwitterDataFetched, setIsTwitterDataFetched, selectedBGColor, setSelectedBGColor, showStats, setShowStats, platform, setPlatform }}>
             {children}
         </AppContext.Provider>
     )
